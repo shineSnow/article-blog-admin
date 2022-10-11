9c9c0023f5221dbeec6747d3d65592b8557a9827
zhangyuanyuan <2264371622@qq.com>
2022-10-11 14:04:34 +0800

    eslint prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -8,7 +8,5 @@ module.exports = {
   trailingComma: 'none', // 最后一个对象元素加逗号
   bracketSpacing: true, // 对象，数组加空格
   jsxBracketSameLine: true, // jsx > 是否另起一行
-  arrowParens: 'always', // (x) => {} 是否要有小括号
-  requirePragma: false, // 不需要写文件开头的 @prettier
-  insertPragma: false // 不需要自动在文件开头插入 @prettier
+  arrowParens: 'always' // (x) => {} 是否要有小括号
 }
